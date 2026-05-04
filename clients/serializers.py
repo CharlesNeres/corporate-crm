cf99942a48bb93be09cf24a54d36b4da3a9e73ff
Charles Oliveira <charlesbritoneresjr@gmail.com>
2026-05-04 23:09:03 +0800

    Add nested tasks to client serializer

--- clients/serializers.py
+++ clients/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from .models import Client, Task
 
-class ClientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        fields = '__all__'
-
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
+        fields = '__all__'
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Client
         fields = '__all__'
